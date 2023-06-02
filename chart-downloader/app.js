0555b5bc29481f9c4f0e434ac0cdc4b3049dffff
gmitis <gi.muths@gmail.com>
2023-06-02 11:51:58 +0300

    Added basic login functionality + errors + middleware

--- chart-downloader/app.js
+++ chart-downloader/app.js
@@ -6,26 +6,58 @@
 // TODO:
 
 const express = require("express");
-const connectDB = require("./db/connectDB");
 const app = express();
+const connectDB = require("./db/connectDB");
+const chartDowload = require("./controllers/chartDownloader");
+const pageNotFound = require("./middleware/pageNotFound");
+const errorHandlerMiddleware = require("./middleware/errorHandlerMiddleware");
+const auth = require("./middleware/authUser");
 
-require('dotenv').config();
+require("dotenv").config();
+const cors = require("cors");
+const Keycloak = require("keycloak-connect");
+const session = require("express-session");
 
 const host = process.env.HOST || "localhost";
 const port = process.env.APP_PORT || 3000;
+const memoryStore = new session.MemoryStore();
+const keycloak = new Keycloak({ store: memoryStore });
 
 // middleware
+app.use(cors());
+
+app.use(
+  session({
+    store: memoryStore,
+    secret: process.env.SECRET,
+    resave: false,
+  })
+);
+
+app.use(
+  keycloak.middleware({
+    logout: "/logout",
+    admin: "/",
+  })
+);
 
 // routes
-app.get("/", (req, res) => res.status(200).send("app is working"));
+app.get("/:email", keycloak.protect("realm:user"), auth, chartDowload);
+app.get("/", keycloak.protect("realm:user"), (req, res) => {
+  res.json({ token: req.kauth.grant.access_token.token });
+});
+app.use("*", pageNotFound);
 
 // error-handler
+app.use(errorHandlerMiddleware);
 
 // spin server
 const spinServer = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, ()=>console.log(`Server listening on http://${host}:${port}`))
+    app.listen(port, () =>
+      console.log(`Server listening on http://${host}:${port}`)
+    );
   } catch (error) {
     console.log(error);
   }
