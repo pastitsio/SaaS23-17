fa536c6fcb0d6bf2f621dea06a08fb1b16af71c7
ntua-el18406 <el18406@mail.ntua.gr>
2023-07-06 19:46:20 +0300

    remove choreographer folder

--- choreographer/test/test-subscriber.js
+++
@@ -1,8 +0,0 @@
-const { consumerCreate } = require("../kafka/kafka-connect");
-const readMessage = require("../kafka/kafka-subscriber");
-
-const consumer = consumerCreate("kafka12", "user-data");
-const parseMessage = (msg) => {
-    console.log(`email: ${msg.email}\ncredits: ${msg.credits}\n`);
-}
-readMessage(consumer, parseMessage);
